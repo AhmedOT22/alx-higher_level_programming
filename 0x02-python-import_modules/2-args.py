77f2e3bfb38d8f8898a431cade70dac84198c13b
Ahmed Ouazzani <Ahmedouazzani225@gmail.com>
2023-08-17 09:23:38 -0700

    Done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     import sys
 
     argc = len(sys.argv) - 1
     if argc == 0:
-        print("0 argument")
+        print("0 argument.")
     elif argc == 1:
         print("1 argument:")
     else:
