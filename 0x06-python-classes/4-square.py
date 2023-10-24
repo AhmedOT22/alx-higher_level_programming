f7cc364fc68173f89ab881b84a781526fe2f9be0
Ahmed Ouazzani <ahmedouazzani225@gmail.com>
2023-10-24 15:32:21 -0700

    Done

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """square class"""
+
+    def __init__(self, size=0):
+        """Initialize the square class
+        Args:
+            size - the size of the square
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: is size is less than 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+        @property
+        def size(self):
+            """retrives the size"""
+
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError('size must be an integer')
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            self.__size = value
+
+    def area(self):
+        """
+        calculate the area of the square
+        Returns: the square of the size
+        """
+
+        return (self.__size ** 2)
