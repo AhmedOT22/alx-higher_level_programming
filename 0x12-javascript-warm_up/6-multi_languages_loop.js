a2f0e8dfb40d222649371befdb4001635f2d9995
AhmedOT22 <Ahmedouazzani225@gmail.com>
2024-06-18 22:25:53 +0000

    Done

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let i;
-const s = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myString = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (i = 0; i < 3; i++) {
-  console.log(s[i]);
+  console.log(myString[i]);
 }
