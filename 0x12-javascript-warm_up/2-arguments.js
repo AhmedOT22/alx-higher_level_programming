5546f4098b9e5d4ff32d0757adfdea70888ca032
AhmedOT22 <Ahmedouazzani225@gmail.com>
2024-06-18 21:40:01 +0000

    Done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const args = process.argv.length;
-console.log(cp === ? "No argument" : cp === 3 ? "Argument found" : "Argument found");
+console.log(cp === 2 ? "No argument" : cp === 3 ? "Argument found" : "Argument found");
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
 const a = Math.floor(Number(process.argv[2]));
-
 if (isNaN(a)) 
 {
   console.log("Missing number of occurences");
