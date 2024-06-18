926c6842f97f25aead14c5c3deadb3ea76a01c73
AhmedOT22 <Ahmedouazzani225@gmail.com>
2024-06-18 22:16:17 +0000

    Done

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const cp = process.argv.length;
-console.log(cp === 2 ? 'No argument' : cp === 3 ? 'Argument found' : 'Argument found');
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Argument found');
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 let i;
 const s = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for (i = 0; i < 3; i++){
+for (i = 0; i < 3; i++) {
   console.log(s[i]);
 }
